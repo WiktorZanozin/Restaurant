ba10086e289b0bfb2ef8e2c42ee5c40edd5829ee
Viktor Zanozin <wiktor.zanozin@gmail.com>
2020-05-04 23:46:03 +0200

    Добавьте файлы проекта.

---
+++ Persistence/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Persistence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
